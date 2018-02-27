6037f3f70ec578a09afad3f6a627130c0248d379
Dustin Dowell <dustindowell22@gmail.com>
2018-02-27 00:52:38 -0600

    remove debug component

--- src/components/Debug.js
+++
@@ -1,8 +0,0 @@
-
-import { h } from 'hyperapp'
-
-const Debug = ([ state ]) =>
-  NODE_ENV === 'development' &&
-    h('div', { class: '_debug' }, JSON.stringify(state.Stash, null, 2))
-
-export { Debug }
