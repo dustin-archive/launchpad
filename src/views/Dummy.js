69ca84eacc4b90a46285e9d2720cb6509b26660c
Dustin Dowell <dustindowell22@gmail.com>
2018-03-31 22:58:22 -0500

    better dummy routes

--- src/views/Dummy.js
+++ src/views/Dummy.js
@@ -3,8 +3,7 @@ import { h } from 'hyperapp'
 
 const Dummy = data =>
   h('div', { class: 'dummy' }, [
-    h('div', { class: 'dummy-text' }, data.text),
-    h('div', null, 'Dummy Route')
+    h('div', null, data.text)
   ])
 
 export default Dummy
