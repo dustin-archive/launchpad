aed962195e72441996d0f091a232c3bfdea0f816
Dustin Dowell <dustindowell22@gmail.com>
2018-04-17 15:34:45 -0500

    udpate

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@
 import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
 
-const { NODE_ENV } = process.env
-
 export default {
   plugins: [
-    replace({ NODE_ENV }),
+    replace({
+      NODE_ENV: process.env.NODE_ENV
+    }),
     resolve()
   ]
 }
